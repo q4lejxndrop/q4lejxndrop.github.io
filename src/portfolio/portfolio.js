b08839dda9c3e4e9241bad63ed41f777c8a6db00
TECNIMUX <145543762+q4lejxndrop@users.noreply.github.com>
2024-12-07 02:37:12 -0500

    Add sounds on buttons nav

--- src/portfolio/portfolio.js
+++ src/portfolio/portfolio.js
@@ -11,21 +11,62 @@ let personalInfo = $$$$('personal-info');
 let projects = $$$$('projects');
 let toolkit = $$$$('toolkit');
 
+const sonido = new Audio('/src/sonidos/Error4.mp3');
+sonido.volume = 0.3;
+sonido.load();
+
+const sonido02 = new Audio('/src/sonidos/gasp-ui-alert.wav');
+sonido.volume = 0.5;
+sonido02.load();
+
 btnAboutMe.addEventListener('click', () => {
 projects.style.display = 'none';
 toolkit.style.display = 'none';
 personalInfo.style.display = 'flex';
+sonido.currentTime = 0;
+sonido.play();
 });
+
+btnAboutMe.addEventListener('mouseenter', () => {
+    sonido02.currentTime = 0;
+    sonido02.play();
+})
 
 btnProjects.addEventListener('click', () => {
 personalInfo.style.display = 'none';
 toolkit.style.display = 'none';
 projects.style.display = 'flex';
+sonido.currentTime = 0;
+sonido.play();
 });
 
-btnToolKit.addEventListener('click', () => {
-    personalInfo.style.display = 'none';
-    projects.style.display = 'none';
-    toolkit.style.display = 'flex';
+btnProjects.addEventListener('mouseenter', () => {
+    sonido02.currentTime = 0;
+    sonido02.play();
 })
 
+btnToolKit.addEventListener('click', () => {
+personalInfo.style.display = 'none';
+projects.style.display = 'none';
+toolkit.style.display = 'flex';
+sonido.currentTime = 0;
+sonido.play();
+})
+
+btnToolKit.addEventListener('mouseenter', () => {
+    sonido02.currentTime = 0;
+    sonido02.play();
+})
+
+
+// Avatar img
+const avatar = $$$$('avatar');
+avatar.addEventListener('click', () => {
+sonido02.currentTime = 0;
+sonido02.play();
+avatar.classList.add('rotate-avatar');
+
+setTimeout(() => {
+    avatar.classList.remove('rotate-avatar');
+}, 2000)
+})

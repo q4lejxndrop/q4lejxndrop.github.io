ce0a4051f710f904cde7288bd715a0c30401131f
TECNIMUX <145543762+q4lejxndrop@users.noreply.github.com>
2024-12-12 01:27:12 -0500

    V3.2

--- src/portfolio/portfolio.js
+++ src/portfolio/portfolio.js
@@ -3,52 +3,102 @@ let $$$ = (selector) => document.querySelectorAll(selector);
 const $$$$ = (selector) => document.getElementById(selector);
 
 // Menu
-const btnAboutMe =  $$$$('btn-about-me');
-// const btnProjects = $$$$('btn-projects');
+const btnHome =  $$$$('btn-home');
+const btnCV = $$$$('btn-cv');
 const btnToolKit = $$$$('btn-toolkit');
 
-// let personalInfo = $$$$('personal-info');
 let projects = $$$$('projects');
 let about = $$$$('about');
 let experience = $$$$('experience');
 let stack = $$$$('stack');
 let toolkit = $$$$('toolkit');
 
-const sonido = new Audio('/src/sonidos/Error4.mp3');
-sonido.volume = 0.3;
-sonido.load();
+const sonido01 = new Audio('/src/sonidos/Error4.mp3');
+sonido01.volume = 0.1;
+sonido01.load();
 
 const sonido02 = new Audio('/src/sonidos/gasp-ui-alert.wav');
-sonido.volume = 0.5;
+sonido02.volume = 0.10;
 sonido02.load();
 
-btnAboutMe.addEventListener('click', () => {
-    about.style.display = 'block';
+// Buttons effects
+const btnEffectsOff = $$$$('btn-effects-off');
+const btnEffectsOn = $$$$('btn-effects-on');
+
+btnEffectsOff.addEventListener('click', () => {
+    btnEffectsOn.style.display = 'block';
+    btnEffectsOff.style.display = 'none';
+    onBtnHome();
+    onBtnCV();
+    onBtnToolKit();
+    onBtnAvatar();
+    sonido02.currentTime = 0;
+    sonido02.play();
+})
+
+btnEffectsOn.addEventListener('click', () => {
+    btnEffectsOff.style.display = 'block';
+    btnEffectsOn.style.display = 'none';
+    offBtnHome();
+    offBtnCV();
+    offBtnToolKit();
+    offBtnAvatar();
+    sonido01.currentTime = 0;
+    sonido01.play();
+})
+
+btnHome.addEventListener('click', () => {
+about.style.display = 'block';
 projects.style.display = 'flex';
 experience.style.display = 'flex';
 stack.style.display = 'flex';
 toolkit.style.display = 'none';
-sonido.currentTime = 0;
-sonido.play();
 });
 
-// btnAboutMe.addEventListener('mouseenter', () => {
-//     sonido02.currentTime = 0;
-//     sonido02.play();
-// })
+function onBtnHome() {
+    btnHome.addEventListener('click', () => {
+        sonido01.currentTime = 0;
+        sonido01.play();
+    })
 
-// btnProjects.addEventListener('click', () => {
-// personalInfo.style.display = 'none';
-// toolkit.style.display = 'none';
-// projects.style.display = 'flex';
-// sonido.currentTime = 0;
-// sonido.play();
-// });
+    btnHome.addEventListener('mouseenter', () => {
+        sonido02.currentTime = 0;
+        sonido02.play();
+    })
+}
 
-// btnProjects.addEventListener('mouseenter', () => {
-//     sonido02.currentTime = 0;
-//     sonido02.play();
-// })
+function offBtnHome() {
+    btnHome.addEventListener('click', () => {
+        sonido01.pause();
+    })
+
+    btnHome.addEventListener('mouseenter', () => {
+        sonido02.pause();
+    })
+}
+
+function onBtnCV() {
+    btnCV.addEventListener('click', () => {
+        sonido01.currentTime = 0;
+        sonido01.play();
+    })
+
+    btnCV.addEventListener('mouseenter', () => {
+        sonido02.currentTime = 0;
+        sonido02.play();
+    })
+}
+
+function offBtnCV() {
+    btnCV.addEventListener('click', () => {
+        sonido01.pause();
+    })
+
+    btnCV.addEventListener('mouseenter', () => {
+        sonido02.currentTime = 0;
+        sonido02.pause();
+    })
+}
 
 btnToolKit.addEventListener('click', () => {
 about.style.display = 'none';
@@ -56,24 +106,51 @@ projects.style.display = 'none';
 experience.style.display = 'none';
 stack.style.display = 'none';
 toolkit.style.display = 'flex';
-sonido.currentTime = 0;
-sonido.play();
 })
 
-btnToolKit.addEventListener('mouseenter', () => {
-    sonido02.currentTime = 0;
-    sonido02.play();
-})
+function onBtnToolKit() {
+    btnToolKit.addEventListener('mouseenter', () => {
+        sonido02.currentTime = 0;
+        sonido02.play();
+    })
 
+    btnToolKit.addEventListener('click', () => {
+        sonido01.currentTime = 0;
+        sonido01.play();
+    })
+}
+
+function offBtnToolKit() {
+    btnToolKit.addEventListener('mouseenter', () => {
+        sonido02.pause();
+    })
+
+    btnToolKit.addEventListener('click', () => {
+        sonido01.currentTime = 0;
+        sonido01.pause();
+    })
+}
 
 // Avatar img
 const avatar = $$$$('avatar');
 avatar.addEventListener('click', () => {
-sonido02.currentTime = 0;
-sonido02.play();
 avatar.classList.add('style-avatar');
 
 setTimeout(() => {
     avatar.classList.remove('style-avatar');
 }, 2000)
 })
+
+function onBtnAvatar() {
+    avatar.addEventListener('click', () => {
+        sonido02.currentTime = 0;
+        sonido02.play();
+    })
+}
+
+function offBtnAvatar() {
+    avatar.addEventListener('click', () => {
+        sonido02.currentTime = 0;
+        sonido02.pause();
+    })
+}
